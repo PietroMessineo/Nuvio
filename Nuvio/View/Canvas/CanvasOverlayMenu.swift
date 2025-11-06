631be2177728a63cc431c948c93387ae8e34ae6b
Pietro <pietromessineo@hotmail.it>
2025-11-06 18:32:43 +0100

    Update design canvas options

--- Nuvio/View/Canvas/CanvasOverlayMenu.swift
+++ Nuvio/View/Canvas/CanvasOverlayMenu.swift
@@ -7,31 +7,83 @@ struct CanvasOverlayMenu: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading, spacing: 11) {
             Button {
                 onPickDocument()
             } label: {
-                Label("Document", systemImage: "folder.fill")
+                CanvasItemView(
+                    image: "document",
+                    title: "Document",
+                    backgroundColor: Color(hex: "#BBE2FF"),
+                    mainColor: Color(hex: "#14598B"),
+                    circleColor: Color(hex: "#80C5FB")
+                )
             }
 
             Button {
-                // TODO: - Open Files App and import PDF
+                // TODO: - Open Text Canvas
             } label: {
-                Label("Text", systemImage: "text.redaction")
+                CanvasItemView(
+                    image: "pencil.and.scribble",
+                    title: "Notes",
+                    backgroundColor: Color(hex: "#FFEABB"),
+                    mainColor: Color(hex: "#8F7434"),
+                    circleColor: Color(hex: "#FBD680")
+                )
             }
             
             Button {
-                // TODO: - Open Files App and import PDF
+                // TODO: - Open Safari View
             } label: {
-                Label("Browser", systemImage: "safari")
+                CanvasItemView(
+                    image: "globe",
+                    title: "Browser",
+                    backgroundColor: Color(hex: "#D9E6E6"),
+                    mainColor: Color(hex: "#366D6C"),
+                    circleColor: Color(hex: "#A8D2D1")
+                )
             }
             
             Button {
-                // TODO: - Open Files App and import PDF
+                // TODO: - Open AI Chat
             } label: {
-                Label("AI", systemImage: "brain")
+                CanvasItemView(
+                    image: "brain",
+                    title: "AI",
+                    backgroundColor: Color(hex: "#CDBBFF"),
+                    mainColor: Color(hex: "#34148B"),
+                    circleColor: Color(hex: "#A080FB")
+                )
             }
         }
+    }
+}
+
+struct CanvasItemView: View {
+    let image: String
+    let title: String
+    let backgroundColor: Color
+    let mainColor: Color
+    let circleColor: Color
+    
+    var body: some View {
+        VStack {
+            Image(systemName: image)
+                .foregroundStyle(mainColor)
+                .font(.system(size: 42, weight: .semibold))
+                .padding(16)
+                .background(circleColor)
+                .clipShape(Circle())
+            
+            Text("Document")
+                .foregroundStyle(mainColor)
+                .fontWeight(.semibold)
+        }
+        .padding(10)
+        .frame(maxWidth: .infinity)
+        .background(backgroundColor)
+        .clipShape(RoundedRectangle(cornerRadius: 31))
+        .padding(.horizontal, 22)
     }
 }
 
